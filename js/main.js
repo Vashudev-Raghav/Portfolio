b71f39b236959ddbd6d6910e538a4df3678ffe15
VASHUDEV-RAGHAV <devraghav81@gmail.com>
2020-04-20 17:52:49 +0530

    first Commit

--- js/main.js
+++ js/main.js
@@ -61,3 +61,13 @@ $(document).ready(function () {
     navbarFixed();
 
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
